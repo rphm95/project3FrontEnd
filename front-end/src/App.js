ea4f6a4ac7512bdb6b29c6aa810b3e007ad700aa
rafaela bootcamp <rafaelabootcamp@rafaelas-air.myfiosgateway.com>
2022-12-12 14:07:33 -0500

    able to add new clothes

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,147 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react'
+import axios from 'axios'
 
-function App() {
+const App = () => {
+
+  // ========
+  // HOOKS 
+  // ========
+
+  const [clothes, setClothes] = useState([])
+  // show
+  const [showAddForm, setShowAddForm] = useState(false);
+  // new Cloth
+  const [newName, setNewName] = useState('')
+  const [newPrice, setNewPrice] = useState()
+  const [newStore, setNewStore] = useState('')
+  const [newImage, setNewImage] = useState('')
+  const [newLink, setNewLink] = useState('')
+  const [newType, setNewType] = useState('')
+
+// ==========
+// get functions
+// ==========
+const getAddForm = () => {
+  setShowAddForm(!showAddForm)
+}
+
+// ==========
+// NEW FORM
+// ==========
+  const handleNewName = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNewPrice = (event) => {
+    setNewPrice(event.target.value)
+  }
+
+  const handleNewStore = (event) => {
+    setNewStore(event.target.value)
+  }
+
+  const handleNewImage = (event) => {
+    setNewImage(event.target.value)
+  }
+
+  const handleNewLink = (event) => {
+    setNewLink(event.target.value)
+  }
+
+  const handleNewType = (event) => {
+    setNewType(event.target.value)
+  }
+
+  // handle new form
+  const handleNewClothes = (event) => {
+    event.preventDefault()
+    axios.post(
+      'http://localhost:3000/boutique',
+      {
+        name: newName,
+        price: newPrice,
+        store: newStore,
+        image: newImage,
+        link: newLink,
+        type: newType
+      }
+    ).then(() => {
+      axios
+        .get('http://localhost:3000/boutique')
+        .then((response) => {
+          setClothes(response.data)
+        })
+    })
+  }
+
+
+
+
+
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3000/boutique')
+      .then((response) => {
+        setClothes(response.data)
+      })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <h1>Welcome to...</h1>
+      <button onClick={getAddForm}>Add Clothes</button>
+
+      {showAddForm ?
+       <>
+        <h2>Add New Item</h2>
+        <div className='container'>
+          <form className="row" onSubmit={handleNewClothes}>
+            
+            <div className="col-md-6">
+              <label className="form-label">Name</label> 
+              <input type="text" className="form-control" onChange={handleNewName} /><br/>
+            </div>
+
+            <div className="col-md-6">
+              <label className="form-label">Name of Store</label>  
+              <input type="text" className="form-control" onChange={handleNewStore} /><br/>
+            </div>
+
+            <div className="col-md-6">
+              <label className="form-label">Price</label>
+              <input type="number" className="form-control" onChange={handleNewPrice}/><br/>
+            </div>
+
+            <div className="col-md-6">
+              <label className="form-label">Type/Category</label>  
+              <input type="text" className="form-control" onChange={handleNewType} /><br/>
+            </div>
+
+            <div className="col-md-12">
+              <label className="form-label">Image Link</label>  
+              <input type="text" className="form-control" onChange={handleNewImage} /><br/>
+            </div>
+
+            <div className="col-md-12">
+              <label className="form-label">Link to Store Site</label>  
+              <input type="text" className="form-control" onChange={handleNewLink} /><br/>
+            </div>
+            
+            <div className="col-md-12"> 
+              <input type="submit" className="form-control btn-info" value="Add New Item" />
+            </div>
+
+          </form>
+        </div>
+       </>  
+      : null }
+
+
+    </main>
+  )
 }
 
 export default App;
