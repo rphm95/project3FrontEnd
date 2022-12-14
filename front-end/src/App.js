e29b56809e10288f61ff89dcefd1c9640555dd06
Mar Mar Min <min.mmar9@gmail.com>
2022-12-13 19:42:55 -0500

    no slash

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -102,7 +102,7 @@ const getLogin = () => {
   const handleNewClothes = (event) => {
     event.preventDefault()
     axios.post(
-      'http://localhost:3000/boutique',
+      'https://gentle-island-40061.herokuapp.com/boutique',
       {
         name: newName,
         price: newPrice,
@@ -113,7 +113,7 @@ const getLogin = () => {
       }
     ).then(() => {
       axios
-        .get('http://localhost:3000/boutique')
+        .get('https://gentle-island-40061.herokuapp.com/boutique')
         .then((response) => {
           setClothes(response.data)
         })
@@ -122,14 +122,14 @@ const getLogin = () => {
 
   const handleNewUser = (event) => {
     axios.post(
-      'http://localhost:3000/users/new',
+      'https://gentle-island-40061.herokuapp.com/users/new',
       {
         username: newUser,
         password: newPassword
       }, {withCredentials:true}
     ).then(() => {
       axios
-        .get('http://localhost:3000/users/new')
+        .get('https://gentle-island-40061.herokuapp.com/users/new')
         .then((response) => {
           console.log(response.data)
         })
@@ -140,14 +140,14 @@ const getLogin = () => {
   const handleLogin = (event) => {
     event.preventDefault()
     axios.post(
-      'http://localhost:3000/sessions/userLogin',
+      'https://gentle-island-40061.herokuapp.com/sessions/userLogin',
       {
         username: newUser,
         password: newPassword
       }, {withCredentials:true}
     ).then(() => {
       axios
-        .get('http://localhost:3000/sessions/new', {withCredentials:true})
+        .get('https://gentle-island-40061.herokuapp.com/sessions/new', {withCredentials:true})
         .then((response) => {
           console.log(response.data)
           setUsers(response.data.username)
@@ -157,10 +157,10 @@ const getLogin = () => {
 
   const handleSessionsDelete = (event) => {
     axios.delete(
-      'http://localhost:3000/sessions/', {withCredentials:true}
+      'https://gentle-island-40061.herokuapp.com/sessions/', {withCredentials:true}
     ).then(() => {
       axios
-        .get('http://localhost:3000/sessions/new')
+        .get('https://gentle-island-40061.herokuapp.com/sessions/new')
         .then((response) => {
           console.log(response)
           setUsers('')
@@ -198,7 +198,7 @@ const getLogin = () => {
   const handleUpdate = (clothesData) => {
     axios
       .put(
-        `http://localhost:3000/boutique/${clothesData._id}`,
+        `https://gentle-island-40061.herokuapp.com/boutique/${clothesData._id}`,
         {
           name: updatedName,
           price: updatedPrice,
@@ -209,7 +209,7 @@ const getLogin = () => {
         }
       ).then((response) => {
         axios
-          .get('http://localhost:3000/boutique')
+          .get('https://gentle-island-40061.herokuapp.com/boutique')
           .then((response) => {
             setClothes(response.data)
           })
@@ -221,10 +221,10 @@ const getLogin = () => {
   // ========
   const handleDelete = (clothesData) => {
     axios
-        .delete(`http://localhost:3000/boutique/${clothesData._id}`)
+        .delete(`https://gentle-island-40061.herokuapp.com/boutique/${clothesData._id}`)
         .then(() => {
           axios
-              .get('http://localhost:3000/boutique')
+              .get('https://gentle-island-40061.herokuapp.com/boutique')
               .then((response) => {
                 setClothes(response.data)
               })
@@ -235,12 +235,12 @@ const getLogin = () => {
 
   useEffect(() => {
     axios
-      .get('http://localhost:3000/boutique')
+      .get('https://gentle-island-40061.herokuapp.com/boutique')
       .then((response) => {
         setClothes(response.data)
       })
       axios
-      .get('http://localhost:3000/sessions/new', {withCredentials:true})
+      .get('https://gentle-island-40061.herokuapp.com/sessions/new', {withCredentials:true})
       .then((response) => {
         setUsers(response.data.username)
         console.log(response.data)
